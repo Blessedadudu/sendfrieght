2b06515753921dd5b5c098fc22340ca30610bae1
Blessedadudu <adududuke@gmail.com>
2022-02-14 09:28:19 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,14 +6,22 @@ const Navbar = () => {
   return (
     <div className='nav flex--2'>
       <p><span>send</span>FREIGHT</p>
-      <div className='searchbar'>
-        <input placeholder='Search'/>
-        <BiSearch className='icon'/>
+      <input type="checkbox" className="nav_checkbox" id="navi-toggle"/> 
+      <label for="navi-toggle" className="nav_button">
+          <span className="iconz">&nbsp;</span>
+      </label>
+      <div className='sub--flex flex--2'>
+        <div className='searchbar'>
+          <input placeholder='Search'/>
+          <BiSearch className='icon'/>
+        </div>
+        <div className='button--double flex--2'>
+          <button>Request Quote</button>
+          <button>Book Shipment</button>
+        </div>
       </div>
-      <div className='button--double flex--2'>
-        <button>Request Quote</button>
-        <button>Book Shipment</button>
-      </div>
+      {/* <div>
+      </div> */}
     </div>
   );
 };
